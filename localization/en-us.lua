ce0ef8e0f305805b41fae9a854dfd6a18c6f3556
Sylvie Nightshade <hi@sylvie.software>
2025-01-07 03:38:41 +0000

    Initial commit

---
+++ localization/en-us.lua
@@ -0,0 +1,21 @@
+return {
+    descriptions = {
+        Stake = {
+            stake_orange = {
+                name = "Orange Stake",
+                text = {
+                    "{C:attention}Booster Packs{} cost",
+                    "{C:money}$1{} more per Ante",
+                    "{s:0.8}Applies all previous Stakes{}"
+                }
+            },
+            stake_gold = {
+                name = "Gold Stake",
+                text = {
+                    "{C:red}-1{} hand size",
+                    "{s:0.8}Applies all previous Stakes{}"
+                }
+            }
+        }
+    }
+}
